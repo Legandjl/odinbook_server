8e08f1f3ca424d746ed77d38682f50f449977350
dorian <dorianlegan@protonmail.com>
2022-05-30 10:47:51 +0100

    basic auth flow implemented

--- app.js
+++ app.js
@@ -3,20 +3,18 @@ const path = require("path");
 const cookieParser = require("cookie-parser");
 const logger = require("morgan");
 const io = require("socket.io")();
-require("./socket")(io);
 const indexRouter = require("./routes/index");
 const postRouter = require("./routes/post");
-const userRouter = require("./routes/user");
-
+const userRouter = require("./routes/users");
+const authRouter = require("./routes/auth");
 const app = express();
-
+const mongoose = require("mongoose");
 require("dotenv").config();
+require("./passport/passport.js");
+require("./socket")(io);
 
 //Set up mongoose connection
-const mongoose = require("mongoose");
-
 const mongoDB = process.env.DB || process.env.MONGODB_URI;
-
 mongoose.connect(mongoDB, { useNewUrlParser: true, useUnifiedTopology: true });
 const db = mongoose.connection;
 db.on("error", console.error.bind(console, "MongoDB connection error:"));
@@ -33,5 +31,6 @@ app.use(express.static(path.join(__dirname, "public")));
 app.use("/", indexRouter);
 app.use("/user", userRouter);
 app.use("/post", postRouter);
+app.use("/auth", authRouter);
 
 module.exports = { app, io };
